0d18e0b0f3b27b7cff82413eb8a9e08763403da6
kasraabdollahi <45338882+kasraabdollahi@users.noreply.github.com>
2022-05-25 20:21:18 +0430

    Add simple initial file

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+  
+class Main {
+    public static void main(String[] args)
+    {
+        int num1 = 10, num2 = 20, sum;
+  
+        // Printing the variables
+        System.out.print("The addition of ");
+        System.out.println(
+            num1 + " and " + num2 + " is:");
+  
+        // Printing the result after operation
+        System.out.println(num1 + num2);
+        
+        if(num1=num2){}
+    }
+}
